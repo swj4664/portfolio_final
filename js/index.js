e3cf94ebb75d37f75bfc8920d43329315439b02f
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-05-31 20:25:58 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let up = document.querySelector('.up')
+let flag = false;
 
 window.addEventListener('scroll', function () {
     let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
@@ -14,26 +15,49 @@ window.addEventListener('scroll', function () {
         up.classList.remove('show')
     }
 
-    if(scrollTop !== 0) {
+    if (scrollTop !== 0) {
         document.querySelector('.scroll').style.opacity = '0'
     } else {
         document.querySelector('.scroll').style.opacity = '1'
     }
 
+    let video = document.getElementById('video')
+    let videoLength = video.getBoundingClientRect()
+    let videoPosition = (videoLength.top + window.pageYOffset)
+    
+console.log(scrollTop)
+console.log('_________')
+console.log(videoPosition+200)
+
+    if(!flag){
+        if (scrollTop > videoPosition-300) {
+            video.autoplay = true
+            video.load();
+            flag = true
+        }
+    } else if (scrollTop < videoPosition-300 || scrollTop > videoPosition+200){
+        flag = false
+        video.autoplay = false
+        video.load();
+    }
+
 });
-document.querySelector('.home').addEventListener('click', ()=> {
+document.querySelector('.home').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
 })
-document.querySelector('.pofol').addEventListener('click', ()=> {
+document.querySelector('.pofol').addEventListener('click', () => {
     let length = document.getElementById('section2').getBoundingClientRect()
-    let position = (length.top+ window.pageYOffset)
-    scrollTo({ top: position-100, behavior: 'smooth' })
+    let position = (length.top + window.pageYOffset)
+    scrollTo({ top: position - 100, behavior: 'smooth' })
 })
-document.querySelector('.skill').addEventListener('click', ()=> {
+document.querySelector('.skill').addEventListener('click', () => {
     let length2 = document.getElementById('section3').getBoundingClientRect()
-    let position2 = (length2.top+ window.pageYOffset)
+    let position2 = (length2.top + window.pageYOffset)
     scrollTo({ top: position2, behavior: 'smooth' })
 })
+
+
+
 
 up.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
@@ -54,13 +78,13 @@ let observer = new IntersectionObserver((e) => {
 let icon = document.querySelectorAll('.icon')
 let iconText = document.querySelectorAll('.icon h3')
 
-for(let i=0; i < icon.length; i++){
+for (let i = 0; i < icon.length; i++) {
     icon[i].addEventListener('mouseover', () => {
         icon[i].style.transform = 'translateY(-20px)'
         iconText[i].style.visibility = 'visible'
         iconText[i].style.opacity = '1'
     })
-    
+
     icon[i].addEventListener('mouseout', () => {
         icon[i].style.transform = 'translateY(0px)'
         iconText[i].style.visibility = 'hidden'
